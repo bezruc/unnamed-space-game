a5bf8627d10b66ec4eb025aaf6710a81c700bcd5
Erich Benes <erich.benes12@gmail.com>
2024-07-09 17:08:37 +0200

    feat: implemented logger

--- main.py
+++ main.py
@@ -1,19 +1,7 @@
-import logging
-import sys
-
 from fastapi import FastAPI
 from unnamed_game import game_instance
 
 app = FastAPI()
-
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.DEBUG)
-stream_handler = logging.StreamHandler(sys.stdout)
-log_formatter = logging.Formatter("%(asctime)s [%(processName)s: %(process)d] [%(threadName)s: %(thread)d] [%(levelname)s] %(name)s: %(message)s")
-stream_handler.setFormatter(log_formatter)
-logger.addHandler(stream_handler)
-
-logger.info('API is starting up')
 
 
 @app.get("/hello")
