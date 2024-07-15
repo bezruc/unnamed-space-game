3c57af69c01bf9d5e8f98a64be20693fd746f9d0
Erich Benes <erich.benes12@gmail.com>
2024-07-15 23:36:48 +0200

    feat: implemented game progression

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from unnamed_game import game_instance
+from unnamed_game import game
 
 app = FastAPI()
 
@@ -10,4 +10,8 @@ async def hello():
 
 @app.get("/start")
 async def start_instance():
-    game_instance.init()
+    game.init(4)
+
+@app.get("/{player}/end_turn")
+async def end_turn(player):
+    game.get_instance().end_turn(player)
--- unnamed_game/src/player.py
+++ unnamed_game/src/player.py
@@ -1,3 +1,6 @@
 class Player:
     def __init__(self) -> None:
-        pass
+        self._turn_ended = False
+
+    def end_turn(self):
+        self._turn_ended = True
